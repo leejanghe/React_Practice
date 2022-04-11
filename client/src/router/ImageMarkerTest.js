954195b34fb8fb0a45885363991ae855396d6d4c
leejanghe <babybye777@gmail.com>
2022-04-11 16:49:58 +0900

    test

--- client/src/router/ImageMarkerTest.js
+++ client/src/router/ImageMarkerTest.js
@@ -16,27 +16,49 @@ width: 30%;
 margin-top: 1rem;
 `
 
+const Marker = styled.div`
+/* width: 100px; */
+height: 20px;
+background-color: #0286ab;
+color: #fff;
+`
+
+const sayHello = () => {
+    console.log('hello')
+}
+
+// const CustomMarker = (a) => {
+//     console.log(1,a)
+//     sayHello();
+//     return (
+//         <Marker>
+//             {a.itemNumber} <input />
+//         </Marker>
+
+//     )
+// }
 
 
 function ImageMarkerTest() {
 
     const [markers, setMarkers] = useState([])
 
-
     return (
         <>
         <Wrapper>
         <ImageMarker
+        style={{backgroundColor: '#blue'}}
         src="https://www.newsworks.co.kr/news/photo/202002/433057_327801_345.jpg"
         markers={markers}
         onAddMarker={(marker) => setMarkers((prev) => [...prev, marker])}
+        // markerComponent={CustomMarker}
         />
 
 <Btn disabled={!markers.length > 0} onClick={() => setMarkers([])}>
         Reset
 </Btn>
 
-<div>ㅇ</div>
+<div>{markers.length}</div>
 
 </Wrapper>
 
