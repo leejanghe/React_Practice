9ac0c2afa991ffd57e9774669aaccb79d6bef446
leejanghe <babybye777@gmail.com>
2022-05-03 00:31:29 +0900

    redux error

--- client/src/components/redux/Detail.js
+++ client/src/components/redux/Detail.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
 
-function Detail({ toDo }) {
+function Detail({ toDos }) {
+
+const myId = useParams().id;
+const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
+
   return (
     <>
       <h1>{toDo?.text}</h1>
@@ -10,13 +15,21 @@ function Detail({ toDo }) {
   );
 }
 
-function mapStateToProps(state, ownProps) {
-  const {
-    match: {
-      params: { id }
+// function mapStateToProps(state, ownProps) {
+//   const {
+//     match: {
+//       params: { id }
+//     }
+//   } = ownProps;
+//   return { toDo: state.find(toDo => toDo.id === parseInt(id)) };
+// }
+function mapStateToProps(state) {
+    return { toDos: state };
     }
-  } = ownProps;
-  return { toDo: state.find(toDo => toDo.id === parseInt(id)) };
-}
 
 export default connect(mapStateToProps)(Detail);
+
+
+// react-router-dom 버전이 6버전부터는 더이상 history, 
+// location, match 등의 props를 component에서 받을 수 없다.
+// useParams 이용해서 진행하기
